c5cfc88643b205dda1aefe1f61a0564fad0e648a
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-03-27 22:08:53 +0200

    created footer (need background)

--- src/App.js
+++ src/App.js
@@ -5,36 +5,41 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 function App() {
   return (
     <React.Fragment>
-      <section className="App">
-        <header>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
-          <ul>
-            <li>
-              <a href="https://github.com/Vaiva11">
-                <FaGithub />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/feed">
-                <FaLinkedinIn />
-              </a>
-            </li>
-          </ul>
-        </header>
-        <main>
-          <h1>Vaiva</h1>
-          <p>
-            The world as we have created it is a process of our thinking. It
-            cannot be changed without changing our thinking. -Albert Einstein
-          </p>
-        </main>
-      </section>
-      {/* <Footer /> */}
+      <div className="All">
+        <section className="App">
+          <header>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Work</li>
+              <li>Contact</li>
+            </ul>
+            <ul>
+              <li>
+                <a href="https://github.com/Vaiva11">
+                  <FaGithub />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/feed">
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </header>
+          <main>
+            <h1>Vaiva</h1>
+            <p>
+              The world as we have created it is a process of our thinking. It
+              cannot be changed without changing our thinking. -Albert Einstein
+            </p>
+          </main>
+        </section>
+        <footer>
+          Junior front-end developer. Laboratory assistant in physical sciences.
+          (https://visme.co/blog/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-04.jpg)
+        </footer>
+      </div>
     </React.Fragment>
   );
 }
