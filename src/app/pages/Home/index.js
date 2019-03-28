88dc96d7c98cb8a3210ed25d7129235651b20a2c
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-03-28 22:01:01 +0200

    added background pic

--- src/app/pages/Home/index.js
+++ src/app/pages/Home/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import '../index.scss';
+import './index.scss';
 
 function Home() {
   return (
     <React.Fragment>
-      <h1>Vaiva</h1>
-      <p>
-        The world as we have created it is a process of our thinking. It cannot
-        be changed without changing our thinking. -Albert Einstein
-      </p>
+      <main>
+        <h1>Vaiva</h1>
+        <p>
+          The world as we have created it is a process of our thinking. It
+          cannot be changed without changing our thinking. -Albert Einstein
+        </p>
+      </main>
     </React.Fragment>
   );
 }
