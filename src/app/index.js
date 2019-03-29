038455c2bc698084f0d82eb6274ba8a1700b7690
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-03-30 00:48:43 +0200

    Added switch & redirect in routing. added About page info and design

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import { Home, About, Work, Contact } from './pages';
 import { Header, Footer } from './components';
 
@@ -9,10 +14,13 @@ function App() {
       <Header />
       <main>
         <Router>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/work" component={Work} />
-          <Route exact path="/contact" component={Contact} />
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/work" component={Work} />
+            <Route exact path="/contact" component={Contact} />
+            <Redirect from="/" to="home" />
+          </Switch>
         </Router>
       </main>
       <Footer />
--- src/app/pages/About/index.js
+++ src/app/pages/About/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
-import '../index.scss';
-
-import { Header, Main, Footer } from '../../components';
+import './index.scss';
 
 function About() {
-  return <React.Fragment>About</React.Fragment>;
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h1>Personal Profile:</h1>
+        <section id="box">
+          <p>
+            Junior front-end developer. Laboratory assistant in physical
+            sciences.
+          </p>
+        </section>
+      </div>
+      <div className="container">
+        <h1>Education:</h1>
+        <section id="box">
+          <h2>Vilnus University</h2>
+          <h3>Computer physics and modelling</h3>
+          <span id="year">Septemper 2015 - Present</span>
+          <p>
+            Advanced mathematics, physics, specialty languages (Lithuanian,
+            English), computer science, introduction to programming (C, C++,
+            JAVA), objective oriented programming, database management (SQL),
+            numerical methods, system software, computer security, introduction
+            to project management.
+          </p>
+
+          <h2>Code Academy</h2>
+          <h3>Front-end developer</h3>
+          <span id="year">October 2018 - Present</span>
+          <p>
+            HTML, CSS / SCSS, Bootstrap, JavaScript, Node.Js, React, Redux,
+            Webpack.
+          </p>
+        </section>
+      </div>
+      <div className="container">
+        <h1>Practice:</h1>
+        <section id="box">
+          <h2>Bite</h2>
+          <h3>Database management</h3>
+          <span id="year">June 2017 - Septemper 2017</span>
+          <p>
+            Database management, meetings with clients and telecommunication
+            improvement.
+          </p>
+        </section>
+      </div>
+      <div className="container">
+        <h1>Work Experience:</h1>
+        <section id="box">
+          <h2>Vilnius University</h2>
+          <h3>Laboratory Assistant</h3>
+          <span id="year">July 2018 - Present</span>
+          <p>Research on dielectric spectroscopy</p>
+        </section>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default About;
